7df15efb3e17461cb6427fe4d7b0b2a684c1767a
Faizan <technicalalam803@gmail.com>
2022-06-02 11:20:15 +0530

    postgresql

--- web/settings.py
+++ web/settings.py
@@ -77,15 +77,17 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default':{
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db1',
         'USER':'postgres',
         'PASSWORD':'Faizan@111',
         'HOST':'Localhost'
-    }
-}
-
+        
+    }}
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
